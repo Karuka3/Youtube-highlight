c542fd00cea5944a41f6c949b1af2c7a80a1f16e
Kazuki Hasegawa <karuka.piplup.0327@gmail.com>
2020-03-31 19:28:22 +0900

    Add test file

---
+++ src/test.py
@@ -0,0 +1,15 @@
+from channel import YoutubeChannel
+import pandas as pd
+
+
+def main():
+    you = YoutubeChannel()
+    #videos = you.get_videos(channel_id="UCCzUftO8KOVkV4wQG1vkUvg")
+    # videos.to_csv("data\\test.csv")
+
+    video_info = you.get_video_info(videoIds=["5Nr41M9zk2c,YSx-pfTthBA"])
+    video_info.to_csv("data\\test_info.csv")
+
+
+if __name__ == "__main__":
+    main()
