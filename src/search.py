a0cfdb59d95ab5278d0de7b32b57c7f641d02c77
Karuka3 <karuka.piplup.0327@gmail.com>
2020-04-06 02:21:34 +0900

    Rename local variables and fix search chat func

--- src/search.py
+++ src/search.py
@@ -1,6 +1,8 @@
 from chat import YoutubeLiveChat
 from channel import YoutubeChannel
 from apiclient.errors import HttpError
+from multiprocessing import Pool
+import multiprocessing as multi
 import itertools
 import pandas as pd
 import os
@@ -21,8 +23,8 @@ def search_videos():
     youchannel = YoutubeChannel()
     try:
         channel_info = pd.read_csv("data\\channel_infos.csv")
-        for id, title in zip(channel_info["channelId"], channel_info["channelTitle"]):
-            videos = youchannel.get_videos(id)
+        for id_, title in zip(channel_info["channelId"], channel_info["channelTitle"]):
+            videos = youchannel.get_videos(id_)
             videos = videos.sort_values(
                 "date", ascending=False).reset_index(drop=True)
             filename = "data\\video_info\\videos_{}.csv".format(title)
@@ -32,24 +34,28 @@ def search_videos():
         print("An HTTP error {} occurred".format(e.resp.status))
 
 
-def search_livechat():
+def search_livechat(filename):
     youchat = YoutubeLiveChat()
-    path = "data\\video_info"
-    files = os.listdir(path)
-    filenames = [f for f in files if os.path.isfile(os.path.join(path, f))]
-    for name in filenames:
-        path = "data\\video_info\\{}".format(name)
-        videos = pd.read_csv(path)
-        videoIds = videos["videoId"]
-        for videoId in videoIds:
+    path = "data\\video_info\\{}".format(filename)
+    dir_path = "data\\livechats\\{}".format(filename.rsplit(".", 1)[0])
+    videos = pd.read_csv(path)
+    for videoId in videos["videoId"]:
+        file_ = dir_path + "\\{}.csv".format(videoId)
+        if os.path.isfile(file_) == False:
             livechat = youchat.get_livechat(videoId)
-            dir_path = "data\\livechats\\{}".format(name.rsplit(".", 1)[0])
-            if os.path.exists(dir_path) == False:
-                os.mkdir(dir_path)
-            livechat.to_csv(dir_path + "\\{}.csv".format(videoId), index=False)
+            livechat.to_csv(file_, index=False)
+        else:
+            pass
 
 
 if __name__ == "__main__":
+    path = "data\\video_info"
+    files = os.listdir(path)
+    filenames = [f for f in files if os.path.isfile(os.path.join(path, f))]
+    n_cores = multi.cpu_count()
+    with Pool(n_cores) as pool:
+        pool.map(search_livechat, filenames)
+
     # search_channels()
     # search_videos()
     # search_livechat()
